a343bf68ba70163252bd8a9872ae19595a747d31
soochan <tncksdl05@gmail.com>
2023-07-14 15:12:50 +0900

    Test(LottoNumberTest): 로또 번호가 가지는 숫자 범위 검증 테스트 추가

---
+++ src/test/java/lotto/domain/LottoNumberTest.java
@@ -0,0 +1,36 @@
+package lotto.domain;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class LottoNumberTest {
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 45})
+    void lottoNumber생성자에는_1이상_45이하의_값만_들어올_수_있다(int number) {
+        assertDoesNotThrow(() -> new LottoNumber(number));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 46})
+    void lottoNumber생성자에는_범위값을_벗어난_숫자가_들어오면_예외가_발생한다(int number) {
+        assertThatThrownBy(() -> new LottoNumber(number))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorMessage.ONLY_LOTTO_NUMBER_IN_RANGE);
+    }
+
+    @Test
+    void lottoNumber는_내부_값이_같으면_같은_객체라고_판단한다() {
+        LottoNumber actualLottoNumber = new LottoNumber(1);
+        LottoNumber expectedLottoNumber = new LottoNumber(1);
+
+        assertThat(actualLottoNumber).isEqualTo(expectedLottoNumber);
+    }
+
+}
