58e3b411ee199179dcca482cddc451bd55925c20
soochan <tncksdl05@gmail.com>
2023-07-14 15:54:59 +0900

    Feat(Lotto): 중복된 숫자가 없는지 검증 코드 추가
    - 예외가 발생한 경우, exception message을 Error Message Class에 추가

--- src/main/java/lotto/domain/ErrorMessage.java
+++ src/main/java/lotto/domain/ErrorMessage.java
@@ -6,6 +6,8 @@ public final class ErrorMessage {
     public static final String ONLY_CAN_DIVIDE_BY_LOTTO_PRICE = "구입 금액은 1000원 단위로 입력 가능합니다.";
     public static final String ONLY_LOTTO_NUMBER_IN_RANGE = "로또 번호는 1 ~ 45 사이의 숫자만 가능합니다.";
 
+    public static final String OVER_LOTTO_SIZE = "로또 번호는 6개만 가능합니다.";
+
     private ErrorMessage() {
     }
 }
--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,20 +1,41 @@
 package lotto.domain;
 
+import static java.util.stream.Collectors.toList;
+import static lotto.domain.ErrorMessage.OVER_LOTTO_SIZE;
+
 import java.util.List;
 
 public class Lotto {
-    private final List<Integer> numbers;
+
+    private static final int LOTTO_SIZE = 6;
+    private final List<LottoNumber> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
-        this.numbers = numbers;
+        this.numbers = convertFrom(numbers);
+    }
+
+    private List<LottoNumber> convertFrom(final List<Integer> numbers) {
+        return numbers.stream()
+                .map(LottoNumber::new)
+                .collect(toList());
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        if (!isRightSize(numbers) || !isUnique(numbers)) {
+            throw new IllegalArgumentException(OVER_LOTTO_SIZE);
         }
     }
 
-    // TODO: 추가 기능 구현
+    private boolean isRightSize(final List<Integer> numbers) {
+        return numbers.size() == LOTTO_SIZE;
+    }
+
+    private boolean isUnique(final List<Integer> numbers) {
+        return numbers.stream()
+                .distinct()
+                .count() == LOTTO_SIZE;
+    }
+
+
 }
