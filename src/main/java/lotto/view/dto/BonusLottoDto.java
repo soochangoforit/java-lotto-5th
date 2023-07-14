4adb1d436c4056232526766443ca3effe1708261
soochan <tncksdl05@gmail.com>
2023-07-15 00:33:13 +0900

    Feat(BonusLottoDto): 숫자가 아닌 경우 예외 처리 추가

--- src/main/java/lotto/view/dto/BonusLottoDto.java
+++ src/main/java/lotto/view/dto/BonusLottoDto.java
@@ -1,8 +1,11 @@
 package lotto.view.dto;
 
+import java.util.regex.Pattern;
+
 public class BonusLottoDto {
 
     private static final int BONUS_LOTTO_SIZE = 1;
+    private static final Pattern NUMBER_PATTERN = Pattern.compile("^[0-9]*$");
     private final int bonusNumber;
 
     private BonusLottoDto(final int bonusNumber) {
@@ -22,9 +25,17 @@ public class BonusLottoDto {
         if (!hasOneNumber(rawBonusLottoNumber)) {
             throw new IllegalArgumentException("보너스 볼은 1개만 입력할 수 있습니다.");
         }
+
+        if (isNotConsistOfNumber(rawBonusLottoNumber)) {
+            throw new IllegalArgumentException("숫자만 입력할 수 있습니다.");
+        }
     }
 
     private static boolean hasOneNumber(final String rawBonusLottoNumber) {
         return rawBonusLottoNumber.length() == BONUS_LOTTO_SIZE;
+    }
+
+    private static boolean isNotConsistOfNumber(final String rawBonusLottoNumber) {
+        return !NUMBER_PATTERN.matcher(rawBonusLottoNumber).matches();
     }
 }
