ddabbb94a7108163f4a83059bcac0ce4491f0e38
soochan <tncksdl05@gmail.com>
2023-07-14 15:49:21 +0900

    Test(RandomNumberGeneratorTest): MethodSource을 활용하여 다양한 케이스 테스트 추가

--- src/test/java/lotto/domain/RandomNumberGeneratorTest.java
+++ src/test/java/lotto/domain/RandomNumberGeneratorTest.java
@@ -3,31 +3,47 @@ package lotto.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
+import java.util.stream.Stream;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class RandomNumberGeneratorTest {
 
     private final NumberGenerator numberGenerator = new RandomNumberGenerator();
 
-    @Test
-    void generateNumbers는_숫자_사이의_값만_생성한다() {
-        List<Integer> numbers = numberGenerator.generateNumbers(1, 45, 6);
-
-        numbers.forEach(number -> assertThat(number).isBetween(1, 45));
+    public static Stream<Arguments> randomNumberGeneratorTestParameters() {
+        return Stream.of(
+                Arguments.of(1, 45, 6),
+                Arguments.of(1, 45, 7),
+                Arguments.of(1, 45, 8),
+                Arguments.of(1, 45, 9),
+                Arguments.of(1, 45, 10)
+        );
     }
 
-    @Test
-    void generateNumbers는_중복되지_않은_값만_생성한다() {
-        List<Integer> numbers = numberGenerator.generateNumbers(1, 45, 6);
+    @ParameterizedTest
+    @MethodSource("randomNumberGeneratorTestParameters")
+    void generateNumbers는_숫자_사이의_값만_생성한다(int min, int max, int size) {
+        List<Integer> numbers = numberGenerator.generateNumbers(min, max, size);
+
+        numbers.forEach(number -> assertThat(number).isBetween(min, max));
+    }
+
+    @ParameterizedTest
+    @MethodSource("randomNumberGeneratorTestParameters")
+    void generateNumbers는_중복되지_않은_값만_생성한다(int min, int max, int size) {
+        List<Integer> numbers = numberGenerator.generateNumbers(min, max, size);
 
         assertThat(numbers).doesNotHaveDuplicates();
     }
 
-    @Test
-    void generateNumbers는_입력받은_크기만큼_값을_생성한다() {
-        List<Integer> numbers = numberGenerator.generateNumbers(1, 45, 6);
+    @ParameterizedTest
+    @MethodSource("randomNumberGeneratorTestParameters")
+    void generateNumbers는_입력받은_크기만큼_값을_생성한다(int min, int max, int size) {
+        List<Integer> numbers = numberGenerator.generateNumbers(min, max, size);
 
-        assertThat(numbers).hasSize(6);
+        assertThat(numbers).hasSize(size);
     }
 }
