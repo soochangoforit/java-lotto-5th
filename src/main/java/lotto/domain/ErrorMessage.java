194cffe38ec2726e68849a9f7bf95abd4d9ad5c4
soochan <tncksdl05@gmail.com>
2023-07-14 23:39:02 +0900

    Feat(WinningLottoDto): 당첨번호가 숫자로만 이루어졌는지 검증 추가

--- src/main/java/lotto/domain/ErrorMessage.java
+++ src/main/java/lotto/domain/ErrorMessage.java
@@ -11,6 +11,7 @@ public final class ErrorMessage {
     public static final String CAN_NOT_UNDER_ZERO = "사고자 하는 개수는 0보다 작을 수 없습니다.";
     public static final String WINNING_LOTTO_NOT_BLANK = "당첨 번호를 입력해 주세요.";
     public static final String IS_NOT_RIGHT_DELIMITER = "구분자는 , 로만 가능합니다.";
+    public static final String IS_NOT_CONSIST_OF_NUMBER = "숫자만 입력 가능합니다.";
 
     private ErrorMessage() {
     }
--- src/main/java/lotto/view/dto/WinningLottoDto.java
+++ src/main/java/lotto/view/dto/WinningLottoDto.java
@@ -1,16 +1,19 @@
 package lotto.view.dto;
 
 import static java.util.stream.Collectors.toList;
+import static lotto.domain.ErrorMessage.IS_NOT_CONSIST_OF_NUMBER;
 import static lotto.domain.ErrorMessage.OVER_LOTTO_SIZE;
 import static lotto.domain.ErrorMessage.WINNING_LOTTO_NOT_BLANK;
 import static lotto.domain.Lotto.LOTTO_SIZE;
 
 import java.util.List;
+import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
 public class WinningLottoDto {
 
     private static final String WINNING_LOTTO_DELIMITER = ",";
+    private static final Pattern numberPattern = Pattern.compile("^[0-9]*$");
 
     private final List<Integer> winningLotto;
 
@@ -37,6 +40,14 @@ public class WinningLottoDto {
         if (isNotLottoSize(rawWinningLottoNumbers)) {
             throw new IllegalArgumentException(OVER_LOTTO_SIZE);
         }
+        if (isNotConsistOfNumber(rawWinningLottoNumbers)) {
+            throw new IllegalArgumentException(IS_NOT_CONSIST_OF_NUMBER);
+        }
+    }
+
+    private static boolean isNotConsistOfNumber(final String rawWinningLottoNumbers) {
+        return Stream.of(rawWinningLottoNumbers.split(WINNING_LOTTO_DELIMITER))
+                .anyMatch(number -> !numberPattern.matcher(number).matches());
     }
 
     private static boolean isNotLottoSize(final String rawWinningLottoNumbers) {
