de05e4c4e941e213906d93750a538067d99142b3
soochan <tncksdl05@gmail.com>
2023-07-18 08:21:10 +0900

    Test(WinningLottoDtoTest): 정상적으로 객체를 생성하는 경우 테스트 추가

--- src/test/java/lotto/view/dto/WinningLottoDtoTest.java
+++ src/test/java/lotto/view/dto/WinningLottoDtoTest.java
@@ -1,7 +1,9 @@
 package lotto.view.dto;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -27,5 +29,14 @@ class WinningLottoDtoTest {
     void from메서드는_6개의_숫자가_아닌_경우_예외를_발생시킨다(String rawWinningLottoNumbers) {
         assertThatThrownBy(() -> WinningLottoDto.from(rawWinningLottoNumbers))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void from메서드는_모든_유효성_검사가_끝나면_객체를_생성한다() {
+        String rawWinningLottoNumbers = "1,2,3,4,5,6";
+
+        WinningLottoDto result = WinningLottoDto.from(rawWinningLottoNumbers);
+
+        assertEquals(WinningLottoDto.class, result.getClass());
     }
 }
